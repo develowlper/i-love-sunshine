abbafd45a40997bc2df507c5a4d32d530a4e1b2f
develowlper <42358670+develowlper@users.noreply.github.com>
2021-01-07 03:01:34 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styled, {
+  x,
+  Preflight,
+  createGlobalStyle
+} from "@xstyled/styled-components";
+
+const Heart = styled.div`
+  position: relative;
+  width: 100px;
+  height: 90px;
+
+  &:before,
+  :after {
+    position: absolute;
+    content: "";
+    left: 50px;
+    top: 0;
+    width: 50px;
+    height: 80px;
+    background: red;
+    border-radius: 50px 50px 0 0;
+    transform: rotate(-45deg);
+    transform-origin: 0 100%;
+  }
+  :after {
+    left: 0;
+    transform: rotate(45deg);
+    transform-origin: 100% 100%;
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`
+  #root {
+    min-width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #root > *:not(:last-child){
+    margin-right: 10px;
+  }
+`;
+
+export default function App() {
+  return (
+    <>
+      <Preflight />
+      <GlobalStyle />
+      <x.div></x.div>
+      <x.div fontSize={90}>I</x.div>
+      <Heart />
+      <x.div fontSize={90}>
+        <span role="img" aria-label="sun emoji">
+          ☀️
+        </span>
+      </x.div>
+    </>
+  );
+}
